ffb9860f3ccd93863ca5fe9369c93d489694a19b
Adriana Ochoa <15171173@itculiacan.edu.mx>
2020-11-18 15:52:48 -0700

    react router dom added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,28 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import MyComponent from "./components/my-component";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import PersonalInfo from "./components/personal-info";
 
 function App() {
   return (
-    <div className="App">
-      <MyComponent name="" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={PersonalInfo} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
