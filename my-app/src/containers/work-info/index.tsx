79b9aad082a944529ff25cb6daea853b49289ea0
Adriana Ochoa <15171173@itculiacan.edu.mx>
2021-01-11 17:23:03 -0700

    Line and workEvent added

--- my-app/src/containers/work-info/index.tsx
+++ my-app/src/containers/work-info/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import WorkEvent from "../../components/work-event";
 import { MyStyledSection, Trapezoid, StyledWorkLine } from "./styled";
 
 function WorkInfo() {
@@ -6,6 +7,11 @@ function WorkInfo() {
     <MyStyledSection>
       <Trapezoid />
       <StyledWorkLine />
+      <WorkEvent
+        year="2017"
+        job="React front end developer"
+        description="I collaborated in the development of a platform for station data deployment of the Instituto Tecnológico de Culiacán"
+      />
     </MyStyledSection>
   );
 }
