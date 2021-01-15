3c720f1e524e9a2fba2decab2347b3f442fb54f7
Adriana Ochoa <15171173@itculiacan.edu.mx>
2021-01-15 16:39:46 -0700

    Contact Me Section finished

--- my-app/src/components/form/styled.tsx
+++ my-app/src/components/form/styled.tsx
@@ -10,11 +10,20 @@ export const FormContainer = styled.div`
   flex-direction: column;
 `;
 
-export const StyledBigTextField = styled(TextField)`
+export const StyledBigTextField = styled.textarea`
   height: 200px;
   margin-bottom: 25px;
   padding: 15px;
   box-sizing: border-box;
+  width: 100%;
+  border-radius: 40px;
+  border: 1px solid #a3a3a3;
+  text-align: left;
+  position: relative;
+  background-color: #ffffff;
+  font-size: 25px;
+  font-family: "roboto";
+  resize: none;
 `;
 
 export const StyledTextField = styled(TextField)`
