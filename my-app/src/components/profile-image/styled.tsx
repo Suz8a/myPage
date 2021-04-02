1bbe9d46ecace73c067a05b57492c7b877671acb
suz8a <suzbel97@gmail.com>
2021-04-01 21:37:07 -0700

    Work experience updated

--- my-app/src/components/profile-image/styled.tsx
+++ my-app/src/components/profile-image/styled.tsx
@@ -1,4 +1,23 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const opacityMoving = keyframes`
+0%{
+opacity:0;
+transform: translate(100px)
+}
+100%{
+opacity:1
+}
+`;
+
+const opacity = keyframes`
+0%{
+opacity:0;
+}
+100%{
+opacity:1
+}
+`;
 
 export const MyImage = styled.img`
   width: auto;
@@ -6,7 +25,11 @@ export const MyImage = styled.img`
   border-radius: 50%;
   margin: auto 0 auto auto;
   grid-area: profileImage;
+  @media (min-width: 1221px) {
+    animation: ${opacityMoving} 1s forwards;
+  }
   @media (max-width: 1220px) {
+    animation: ${opacity} 1s forwards;
     margin: auto;
   }
 `;
--- my-app/src/content/section-work-info.tsx
+++ my-app/src/content/section-work-info.tsx
@@ -1,20 +1,26 @@
 export const workExperience = [
   {
     year: "2017",
-    job: "React front end developer",
+    job: "Frontend developer",
     description:
       "I collaborated in the development of a platform for station data deployment of the Instituto Tecnológico de Culiacán",
   },
   {
     year: "2019",
-    job: "Full stack",
+    job: "Fullstack",
     description:
-      "I planned and developed a school project of a control system of a jewelry store ",
+      "I planned and developed a school project of a control system of a jewelry store",
   },
   {
     year: "2020",
-    job: "Vue front end developer",
+    job: "Frontend developer",
     description:
       "Worked 1 year as a Vue js front end developer for Instituto Tecnológico de Culiacán",
+  },
+  {
+    year: "2021 - Present",
+    job: "Frontend developer at albo",
+    description:
+      "Implementing new features, creating reusable components and fixing bugs",
   },
 ];
